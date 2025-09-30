4acae7fca6d6c44fbc5c1e7daefc3d7554b88de9
Gaozhizheng <59356796+Gaozhizheng@users.noreply.github.com>
2025-09-30 14:55:12 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+new CozeWebSDK.AppWebSDK({
+  token: 'cztei_hdxpjDwCX0qA6swxBmPDsBmaj3BKDCpZXwgVRcJdHTc8EMA0QzcYLHqcP65eM032l',
+  appId: '7535228740545183796',
+  container: '#app',
+  userInfo: {
+    id: 'user',
+    url: 'https://lf-coze-web-cdn.coze.cn/obj/eden-cn/lm-lgvj/ljhwZthlaukjlkulzlp/coze/coze-logo.png',
+    nickname: 'User',
+  },
+  ui: {
+    className: '',
+  }
+});
